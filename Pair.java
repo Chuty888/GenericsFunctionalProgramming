656659d16e1f7308c5cb560e9b96d85ff4dacd90
Chuty888 <chutima.bod@ku.th>
2026-09-17 17:23:06 +0700

    Add files via upload

--- Pair.java
+++ Pair.java
@@ -1,10 +1,29 @@
 /**
  * TODO: ทำให้คลาสนี้เป็น Generic <K, V> ที่สามารถเก็บอ็อบเจกต์ได้ 2 ชนิด
  */
-public class Pair {
+public class Pair<k,v> {
     // TODO: สร้างฟิลด์ private final สำหรับ key และ value
+     private  final k key;
+     private  final v value;
     
     // TODO: สร้าง Constructor ที่รับ key และ value
 
+   
+   public  Pair (k key, v value){
+       
+        this.key = key;
+        this.value = value;
+
+       }
+      
+
     // TODO: สร้าง Getters สำหรับ key และ value
+
+    public  k getKey(){
+        return key;
+    }
+
+    public  v getValue(){
+        return value;
+    }
 }
